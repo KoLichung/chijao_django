830138e405c5633a46c0892ea77335a047992231
WenJenChun <daydreamsss@hotmail.com>
2022-10-06 13:55:00 +0800

    add web

--- app/web/urls.py
+++ app/web/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('privacy_policy_others', views.privacy_policy_others, name='privacy_policy_others'),
+    path('index', views.index, name='index'),
+    path('price', views.price, name='price'),
+    path('works', views.works, name='works'),
+    path('contact', views.contact, name='contact'),
+    path('message_sent', views.message_sent, name='message_sent'),
+
 ]
--- app/web/views.py
+++ app/web/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def privacy_policy_others(request):
-    return render(request,'site/privacy_policy_others.html',)
+    return render(request,'web/privacy_policy_others.html',)
+
+def index(request):
+    return render(request,'web/index.html')
+
+def price(request):
+    return render(request,'web/price.html')
+
+def works(request):
+    return render(request,'web/works.html')
+
+def contact(request):
+    return render(request,'web/contact.html')
+
+def message_sent(request):
+    return render(request,'web/message_sent.html')
