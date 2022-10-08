8c0ca34ae05d52a5790e9552702d11da4b59ed5d
WenJenChun <daydreamsss@hotmail.com>
2022-10-08 17:18:42 +0800

    add mailchimp api

--- app/mailchimp/tasks.py
+++ app/mailchimp/tasks.py
@@ -1,6 +1,40 @@
+import mailchimp_transactional as MailchimpTransactional
+from mailchimp_transactional.api_client import ApiClientError
 
-def test():
-    print('test')
+def run():
+  try:
+    mailchimp = MailchimpTransactional.Client('SQtcqkSt9MApMYOlAcvrQQ')
+    response = mailchimp.users.ping()
+    print('API called successfully: {}'.format(response))
+  except ApiClientError as error:
+    print('An exception occurred: {}'.format(error.text))
+
+run()
+
+
 
 def test_mail_to_jason():
     print('send mail to jason@kosbrother.com')
+
+    mailchimp = MailchimpTransactional.Client('SQtcqkSt9MApMYOlAcvrQQ')
+    message = {
+        "from_email": "manny@mailchimp.com",
+        "subject": "Hello world",
+        "text": "Welcome to Mailchimp Transactional!",
+        "to": [
+        {
+            "email": "freddie@example.com",
+            "type": "to"
+        }
+        ]
+    }
+
+    def run():
+      try:
+        response = mailchimp.messages.send({"message":message})
+        print('API called successfully: {}'.format(response))
+      except ApiClientError as error:
+        print('An exception occurred: {}'.format(error.text))
+
+    run()
+
